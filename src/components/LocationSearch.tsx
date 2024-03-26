669e028d9fad3ea293a6c4b90f2acb80d0686f48
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-26 14:13:13 +0800

    Receive term input as state

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -7,10 +7,23 @@ interface LocationSearchProps {
 
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
 
+    const [term, setTerm] = useState('');
     const [place, setPlace] = useState<Place[]>([]);
 
     return (
-        <div>Search for a location!</div>
+        <div>
+            <form>
+                <label className="font-bold" htmlFor="term">
+                    Search
+                </label>
+                <input
+                    className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+                    id="term"
+                    value={term}
+                    onChange={event => setTerm(event.target.value)}
+                />
+            </form>
+        </div>
     );
 
 }
