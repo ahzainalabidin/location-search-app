8ea27f9088ac0151e1b0d1480639ad68f5838a2f
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-26 14:58:43 +0800

    Define type for search response

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,10 +1,22 @@
+interface SearchResponse {
+    features: {
+        geometry: {
+            coordinates: number[];
+        };
+        properties: {
+            place_id: number;
+            display_name: string;
+        };
+    }[];
+}
+
 export const search = async (term: string) => {
 
     const res = await fetch(
         `https://nominatim.openstreetmap.org/search?q=${term}&format=geojson&addressdetails=1&layer=address&limit=5`
     );
 
-    const data = await res.json();
+    const data: SearchResponse = await res.json();
     console.log(data)
 
 };
