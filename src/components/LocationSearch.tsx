ba2da5cf2e42cccfef0cbc48ca96535c76ac4864
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-26 15:21:49 +0800

    Display list of places name

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import type { Place } from "../api/Place";
 import { search } from "../api/search";
 
@@ -9,19 +9,19 @@ interface LocationSearchProps {
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
 
     const [term, setTerm] = useState('');
-    const [place, setPlace] = useState<Place[]>([]);
+    const [places, setPlaces] = useState<Place[]>([]);
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        await search(term);
+        const results = await search(term);
+        setPlaces(results);
     };
 
     return (
         <div>
+
             <form onSubmit={handleSubmit}>
-                <label className="font-bold" htmlFor="term">
-                    Search
-                </label>
+                <label className="font-bold" htmlFor="term">Search</label>
                 <input
                     className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
                     id="term"
@@ -29,6 +29,28 @@ export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
                     onChange={event => setTerm(event.target.value)}
                 />
             </form>
+
+            <h1 className="font-bold mt-6">Found Locations</h1>
+
+            <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
+                {
+                    places.map(place => {
+                        return (
+                            <Fragment key={place.id}>
+                                <p className="text-sm">{place.name}</p>
+                                <button
+                                    className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded"
+                                    onClick={() => onPlaceClick(place)}
+                                >
+                                    Go
+                                </button>
+                                <div className="border-b w-full col-span-2" />
+                            </Fragment>
+                        );
+                    })
+                }
+            </div>
+
         </div>
     );
 
