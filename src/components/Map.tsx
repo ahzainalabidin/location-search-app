78c43ce4f2a01f0d8f51ae0a5dc6df1d894bb8ed
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-26 15:50:35 +0800

    Add map with marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,8 @@
+import 'leaflet/dist/leaflet.css';
 import type { Place } from "../api/Place";
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { useEffect, useRef } from 'react';
+import { Map as LeafletMap } from 'leaflet';
 
 interface MapProps {
     place: Place | null;
@@ -6,8 +10,25 @@ interface MapProps {
 
 export default function Map({ place }: MapProps) {
 
+    const mapRef = useRef<LeafletMap | null>(null);
+
+    useEffect(() => {
+        if (mapRef.current && place) {
+            mapRef.current.flyTo([place.latitude, place.longitude]);
+        }
+    }, [place]);
+
     return (
-        <div>Map!!</div>
+        <MapContainer
+            ref={mapRef}
+            center={[40.7, -74]}
+            zoom={12}
+            scrollWheelZoom
+            className='h-full'
+        >
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            {place && <Marker position={[place.latitude, place.longitude]} />}
+        </MapContainer>
     );
 
 }
